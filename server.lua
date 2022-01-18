ea2e7ff05179695ca790664c96ce61b469e27f03
Adzeepulse <nathan@nathanlaw.co.uk>
2022-01-18 14:21:12 +0000

    Update server.lua for items

--- server.lua
+++ server.lua
@@ -64,6 +64,34 @@ ESX.RegisterServerCallback('ap_createjob:createJob',function(source, cb, jobName
 	end
 end)
 
+ESX.RegisterServerCallback('ap_createjob:createItem',function(source, cb, itemName, itemLabel, itemWeight, itemRare, itemCanRemove)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	local name = xPlayer.getName()
+    if (itemName and itemLabel and itemWeight and itemRare and itemCanRemove) then
+		MySQL.Async.fetchAll(
+			'INSERT INTO items(name, label, weight, rare, can_remove) VALUES (@itemName, @itemLabel, @itemWeight, @itemRare, @itemCanRemove);',
+			{
+				['@itemName'] = itemName,
+				['@itemLabel'] = itemLabel,
+				['@itemWeight'] = itemWeight,
+				['@itemRare'] = itemRare,
+				['@itemCanRemove'] = itemCanRemove
+			}, function(result)
+				if result then		
+					TriggerClientEvent('ap_createjob:notify', xPlayer.source, 'Successful')
+					sendLogsDiscord(source, name, "Item name: "..itemName.." | Item label: "..itemLabel.." | Item weight: "..itemWeight.." | Item rare: "..itemRare.." | Item can_remove: "..itemCanRemove.." | Added to the database.")
+					cb(true)
+				else
+					TriggerClientEvent('ap_createjob:notify', xPlayer.source, 'Couldnt create item (database error)')
+					cb(false)
+				end
+			end)
+	else
+		TriggerClientEvent('ap_createjob:notify', xPlayer.source, 'Couldnt create item, argument missing')
+		cb(false)
+	end
+end)
+
 RegisterServerEvent("ap_createjob:RefreshJobs")
 AddEventHandler("ap_createjob:RefreshJobs", function()
 	local xPlayer = ESX.GetPlayerFromId(source)
@@ -72,6 +100,16 @@ AddEventHandler("ap_createjob:RefreshJobs", function()
     TriggerClientEvent('ap_createjob:notify', xPlayer.source, 'Jobs have been refreshed!')
 	sendLogsDiscord(source, name, "Jobs have been refreshed!")
 	TriggerClientEvent('ap_createjob:createjobMain', xPlayer.source)
+end)
+
+RegisterServerEvent("ap_createjob:RefreshItems")
+AddEventHandler("ap_createjob:RefreshItems", function()
+    local xPlayer = ESX.GetPlayerFromId(source)
+    local name = xPlayer.getName()
+    ESX.RefreshItems()
+    TriggerClientEvent('ap_createjob:notify', xPlayer.source, 'Items have been refreshed!')
+    sendLogsDiscord(source, name, "Items have been refreshed!")
+    TriggerClientEvent('ap_createjob:createitemMain', xPlayer.source)
 end)
 
 sendLogsDiscord = function(color, name, message)
