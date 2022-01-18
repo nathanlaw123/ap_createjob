7a1a1e93d77ee0ef0e2dda516f8f252462f193f7
Adzeepulse <nathan@nathanlaw.co.uk>
2022-01-18 14:24:39 +0000

    Update client.lua for items.

--- client.lua
+++ client.lua
@@ -22,13 +22,45 @@ function checkAdmin()
 	ESX.TriggerServerCallback('ap_createjob:getAdmin', function(data)
 		for k,v in pairs(data) do
 			if v.group == adminGrades then
-				TriggerEvent('ap_createjob:createjobMain')
+				TriggerEvent('ap_createjob:apadminmenu')
 			else
 				ESX.ShowNotification('You are not authorised to use this command!')
 			end
 		end
 	end)
 end
+
+RegisterNetEvent('ap_createjob:apadminmenu')
+AddEventHandler('ap_createjob:apadminmenu', function()
+	local adminMain = {
+        {
+            id = 1,
+            header = 'AP Admin Menu',
+            txt = ''
+        },
+        {
+            id = 2,
+            header = 'Jobs Menu',
+            txt = 'Click for jobs menu.',
+            params = {
+                event = 'ap_createjob:createjobMain',
+                isServer = false,
+                args = {}
+            }
+        },
+		{
+            id = 3,
+            header = 'Items Menu',
+            txt = 'Click for items menu.',
+            params = {
+                event = 'ap_createjob:createitemMain',
+                isServer = false,
+                args = {}
+            }
+        }
+    }
+	exports['zf_context']:openMenu(adminMain)
+end)
 
 RegisterNetEvent('ap_createjob:createjobMain')
 AddEventHandler('ap_createjob:createjobMain', function()
@@ -65,6 +97,16 @@ AddEventHandler('ap_createjob:createjobMain', function()
             params = {
                 event = 'ap_createjob:RefreshJobs',
                 isServer = true,
+                args = {}
+            }
+        },
+        {
+            id = 5,
+            header = 'Back',
+            txt = 'Back to main menu.',
+            params = {
+                event = 'ap_createjob:apadminmenu',
+                isServer = false,
                 args = {}
             }
         }
@@ -142,6 +184,89 @@ AddEventHandler('ap_createjob:addgrade', function()
 					TriggerEvent('ap_createjob:addgrade')
 				end
 			end, dialog[1].input, dialog[3].input, dialog[4].input, dialog[2].input, dialog[5].input)
+        end
+    end
+end)
+
+RegisterNetEvent('ap_createjob:createitemMain')
+AddEventHandler('ap_createjob:createitemMain', function()
+	local itemMain = {
+        {
+            id = 1,
+            header = 'Create Items',
+            txt = ''
+        },
+        {
+            id = 2,
+            header = 'Add Items',
+            txt = 'Add items to database.',
+            params = {
+                event = 'ap_createjob:additem',
+                isServer = false,
+                args = {}
+            }
+        },
+		{
+            id = 3,
+            header = 'Refresh Items',
+            txt = 'Click to refresh items table.',
+            params = {
+                event = 'ap_createjob:RefreshItems',
+                isServer = true,
+                args = {}
+            }
+        },
+        {
+            id = 4,
+            header = 'Back',
+            txt = 'Back to main menu.',
+            params = {
+                event = 'ap_createjob:apadminmenu',
+                isServer = false,
+                args = {}
+            }
+        }
+    }
+	exports['zf_context']:openMenu(itemMain)
+end)
+
+RegisterNetEvent('ap_createjob:additem')
+AddEventHandler('ap_createjob:additem', function()
+	local dialog = exports['zf_dialog']:DialogInput({
+        header = "Make New Item", 
+        rows = {
+            {
+                id = 0, 
+                txt = "Item name (lowercase)"
+            },
+            {
+                id = 1, 
+                txt = "Item label"
+            },
+			{
+                id = 2, 
+                txt = "Item weight"
+            },
+            {
+                id = 3, 
+                txt = "Item rare"
+            },
+            {
+                id = 4, 
+                txt = "Item can_remove"
+            },
+        }
+    })   
+    if dialog ~= nil then
+        if dialog[1].input == nil or dialog[2].input == nil or dialog[3].input == nil or dialog[4].input == nil or dialog[5].input == nil then
+            ESX.ShowNotification('All fields need to be filled.')
+        else
+            ESX.TriggerServerCallback('ap_createjob:createItem', function(hasItem)
+				if hasItem then
+					ESX.ShowNotification(' Item label: '..dialog[2].input..' has been added.')
+					TriggerEvent('ap_createjob:createitemMain')
+				end
+			end, dialog[1].input, dialog[2].input, dialog[3].input, dialog[4].input, dialog[5].input)
         end
     end
 end)
